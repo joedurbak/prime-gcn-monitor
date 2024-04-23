4d5640f0785f8ba975796a6a568cb5c84dd1fec2
joe durbak <durbak.3@gmail.com>
2024-04-23 00:39:42 -0400

    updated calculate_grid_location.py to use the offset grid

--- calculate_grid_location.py
+++ calculate_grid_location.py
@@ -17,11 +17,12 @@ warnings.filterwarnings('ignore', category=SettingWithCopyWarning)
 # target_coords = SkyCoord(165.4746, 9.9258, unit=u.deg)  # in the gap
 # target_coords = SkyCoord(161.5702, 8.8237, unit=u.deg)  # in the gap
 # target_coords = SkyCoord(157.5978, 4.1621, unit=u.deg)
-# target_coords = SkyCoord('06:17:11.7864', '-44:44:39.876', unit=(u.hourangle, u.deg))
-target_coords = SkyCoord(94.5677, -45.0114, unit=u.deg)
+target_coords = SkyCoord('06:17:11.7864', '-44:44:39.876', unit=(u.hourangle, u.deg))
+# target_coords = SkyCoord(94.5677, -45.0114, unit=u.deg)
 # 	23h25m54.33s	-55d07m55.20s
 # grid_df = read_csv('PRIME_sq_r1.2deg_packing.tess', sep=' ')
 grid_df = read_csv('obsable_all_sky_grid.csv', sep=',')
+offset_grid_df = read_csv('offset_obsable_all_sky_grid.csv', sep=',')
 output_name = 'GRB240419a.csv'
 
 
@@ -66,13 +67,13 @@ def calculate_distance_all(target=target_coords, grid=grid_df):
     min_dist = np.min(d_arr)
     index = np.where(d_arr == min_dist)
     # print(d_arr)
-    grid_df['distance'] = d_arr
-    grid_df['ra_offsets'] = np.asarray(ra_offsets)
-    grid_df['dec_offsets'] = np.asarray(dec_offsets)
-    grid_df['ra_degrees'] = np.asarray(ra_degrees)
-    grid_df['dec_degrees'] = np.asarray(dec_degrees)
-    grid_df['chip'] = np.asarray(chips)
-    return index, min_dist, grid_df
+    grid['distance'] = d_arr
+    grid['ra_offsets'] = np.asarray(ra_offsets)
+    grid['dec_offsets'] = np.asarray(dec_offsets)
+    grid['ra_degrees'] = np.asarray(ra_degrees)
+    grid['dec_degrees'] = np.asarray(dec_degrees)
+    grid['chip'] = np.asarray(chips)
+    return index, min_dist, grid
 
 
 def generate_point_source_csv(dataframe):
@@ -114,12 +115,18 @@ def generate_point_source_csv(dataframe):
     return new_df
 
 
-def generate_observation_csv(target, save_name, grid=grid_df, tile_radius=None):
+def generate_observation_csv(target, save_name, grid=grid_df, backup_grid=offset_grid_df, tile_radius=None):
     i, dist, new_df = calculate_distance_all(target, grid)
     # new_df.to_csv('PRIME.tess', columns=['ObjectName', 'ra_degrees', 'dec_degrees'], sep=' ', index=False)
     new_df = new_df.sort_values('distance')
     if tile_radius is None:
-        new_df = generate_point_source_csv(new_df)
+        try:
+            new_df = generate_point_source_csv(new_df)
+        except ValueError:
+            print('source is in the grid gaps, switching to offset grid')
+            i, dist, new_df = calculate_distance_all(target, backup_grid)
+            new_df = new_df.sort_values('distance')
+            new_df = generate_point_source_csv(new_df)
     else:
         new_df = new_df.loc[new_df['distance'] < tile_radius]
         new_df['Comment1'] = new_df['distance']
